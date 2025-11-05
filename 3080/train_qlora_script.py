b21a3e75ca2f6afc2069423e0fbd99ed12ea2d1b
protove <sbdl12342@gmail.com>
2025-11-05 16:01:54 +0900

    background_training.py

--- 3080/train_qlora_script.py
+++ 3080/train_qlora_script.py
@@ -106,7 +106,7 @@ class ExperimentManager:
             "LoraConfig": {
                 "r": lora_config.r,
                 "lora_alpha": lora_config.lora_alpha,
-                "target_modules": lora_config.target_modules,
+                "target_modules": list(lora_config.target_modules) if isinstance(lora_config.target_modules, set) else lora_config.target_modules,
                 "lora_dropout": lora_config.lora_dropout,
                 "bias": lora_config.bias,
                 "task_type": lora_config.task_type,
